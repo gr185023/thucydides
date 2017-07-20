ee370b48aa74aa50f5aa734154a912923483bb0c
roven <roven.galeon@truste.com>
2017-07-20 15:52:11 +0800

    swiper pagination progress

--- thucydides/app/js/main.js
+++ thucydides/app/js/main.js
@@ -12,11 +12,73 @@ Primary use: App & Mobile Website
 
 "use strict";
 
-( function( $ ) {
+// ( function( $ ) {
 
+//     $( '.swipebox' ).swipebox();
+
+// } )( jQuery );
+
+// This script prevents links from opening in Mobile Safari. https://gist.github.com/1042026
+(function(a, b, c) {
+    if (c in b && b[c]) {
+        var d, e = a.location,
+            f = /^(a|html)$/i;
+        a.addEventListener("click", function(a) {
+            d = a.target;
+            while (!f.test(d.nodeName)) d = d.parentNode;
+            "href" in d && (d.href.indexOf("http") || ~d.href.indexOf(e.host)) && (a.preventDefault(), e.href = d.href)
+        }, !1)
+    }
+})(document, window.navigator, "standalone")
+
+// SmoothState
+var duration_CONSTANT = 250;
+var options = {
+    prefetch: true,
+    cacheLength: 0,
+    blacklist: '.no-smoothState',
+    onStart: {
+        duration: duration_CONSTANT,
+        render: function ($container) {
+            $('#bottom-sheet').closeModal();
+            $container.addClass('is-exiting');
+            smoothState.restartCSSAnimations();
+            setTimeout(function () { }, duration_CONSTANT * 2);
+        }
+    },
+    onReady: {
+        duration: 0,
+        render: function ($container, $newContent) {
+            $container.removeClass('is-exiting');
+            $container.html($newContent);
+        }
+    },
+    onAfter: function ($container, $newContent) {
+        setTimeout(function () {
+            ResizeHandler = ResizeHandler || function () { };
+            ResizeHandler();
+        }, 500)
+        initiate_plugins(); // All onAfter calls goes inside this function
+    }
+};
+var smoothState = $('#main').smoothState(options).data('smoothState');
+
+/*------------------------------------------------------------------
+    Call all function for Ajax
+-------------------------------------------------------------------*/
+function initiate_plugins() {
     $( '.swipebox' ).swipebox();
 
-} )( jQuery );
+    // Sliders
+var swiper = new Swiper('.swiper-slider', { // Default
+    pagination: '.swiper-pagination',
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    // autoplay: false,
+    // loop: true,
+    paginationType: 'progress',
+});
+}
 
 // This script prevents links from opening in Mobile Safari. https://gist.github.com/1042026
 (function(a, b, c) {
@@ -45,26 +107,57 @@ $('#open-left').sideNav({ // Trigger id
     closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
 });
 
+// /*------------------------------------------------------------------
+//     Call all function for Ajax
+// -------------------------------------------------------------------*/
+// function initiate_plugins() {
+//     // Sliders
+//     var swiper = new Swiper('.swiper-container', { // Default
+//         // pagination: '.swiper-pagination',
+//         // paginationClickable: false,
+//         // nextButton: '.swiper-button-next',
+//         // prevButton: '.swiper-button-prev',
+//         autoplay: true,
+//         loop: true,
+//         paginationType: 'progress',
+//         // autoplayDisableOnInteraction: true,
+//         parallax: true,
+//         speed: 4000,
+//         watchSlidesProgress: true,
+//         loopedSlides: 2,
+// });
+
+// }
+
+// // Sliders
+// var swiper = new Swiper('.swiper-slider', { // Default
+//     pagination: '.swiper-pagination',
+//     // paginationClickable: false,
+//     // nextButton: '.swiper-button-next',
+//     // prevButton: '.swiper-button-prev',
+//     // autoplay: true,
+//     loop: true,
+//     paginationType: 'progress',
+//     // autoplayDisableOnInteraction: true,
+//     parallax: true,
+//     speed: 600,
+//     watchSlidesProgress: true,
+//     loopedSlides: 1,
+//     slidesPerView: 1,
+//     loopAdditionalSlides: 0,
+// });
+
 // Sliders
 var swiper = new Swiper('.swiper-slider', { // Default
     pagination: '.swiper-pagination',
-    paginationClickable: true,
     nextButton: '.swiper-button-next',
     prevButton: '.swiper-button-prev',
     autoplay: true,
     loop: true,
     paginationType: 'progress',
-    autoplayDisableOnInteraction: true,
-    parallax: true
+    speed: 3000,
 });
 
-// init swiper layout
-window.onload = function () {
-    setTimeout(function () {
-        ResizeHandler = ResizeHandler || function () { };
-        ResizeHandler();
-    }, 500)
-};
 
 // Add Primary Color To Header After Scroll Down
 $(function() {
@@ -81,15 +174,12 @@ $(function() {
     });
 });
 
-// MixItUP
+// MixItUp
 $(function () {
     var layout = 'grid', // Store the current layout as a variable
         $container = $('#filter'), // Cache the MixItUp container
         $changeLayout = $('#ChangeLayout'); // Cache the changeLayout button
     // Instantiate MixItUp with some custom options:
-    try {
-        $container.mixItUp('destroy');
-    } catch (x) { }
     $container.mixItUp({
         animation: {
             animateChangeLayout: true, // Animate the positions of targets as the layout changes
@@ -97,7 +187,7 @@ $(function () {
             effects: 'fade rotateX(-40deg) translateZ(-100px)'
         },
         layout: {
-            containerClass: 'grid' // Add the class 'grid' to the container on load
+            containerClass: 'grid' // Add the class 'list' to the container on load
         }
     });
     // MixItUp does not provide a default "change layout" button, so we need to make our own and bind it with a click handler:
@@ -118,5 +208,16 @@ $(function () {
             });
         }
     });
+  
+    // init swiper layout
+    window.onload = function () {
+        setTimeout(function () {
+            ResizeHandler = ResizeHandler || function () { };
+            ResizeHandler();
+        }, 500)
+    };
+
 });
+
+
 
